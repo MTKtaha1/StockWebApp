28bcd4fc007e45239075784be0bdce1b950e39c2
Sahil <saaahil.sethi@gmail.com>
2024-04-10 01:07:03 -0400

    Almost Done

--- myApp.py
+++ myApp.py
@@ -1,4 +1,7 @@
+import time
 from datetime import datetime, timedelta, date
+import folium
+import requests
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,27 +10,56 @@ import plotly.express as px
 import numpy as np
 import finnhub
 from statsmodels.tsa.arima.model import ARIMA
-from plotly import graph_objs as go
-
-
+from streamlit_folium import folium_static
 from streamlit_option_menu import option_menu
 
 # Setup Finnhub client with your API key
 finnhub_client = finnhub.Client(api_key="co8vr39r01qj5gtjfu0gco8vr39r01qj5gtjfu10")
 st.sidebar.title("Stocks Web App")
-option = st.sidebar.selectbox("Select an Option", ["Home", "Beginner", "Intermediate", "Expert"])
-
-option2 = st.sidebar.selectbox("Stocks Prediction", ["Stocks", "Map"])
 
 
-def get_session_state():
-    if 'section' not in st.session_state:
-        st.session_state.section = "Home"
-    return st.session_state
+def home():
+
+    st.write("Please answer the following questions:")
+
+    # Question 1: Radio Buttons
+    q1 = st.radio("1. Do you have prior experience in trading stocks?", ("Yes", "No"))
+
+    # Question 2: Slider
+    q2 = st.slider("2. On a scale of 1 to 10, how confident are you in trading stocks?", 1, 10, 5)
+
+    # Question 3: Radio Buttons
+    q3_options = ["Beginner", "Intermediate", "Expert"]
+    q3 = st.radio("3. What is your level of expertise in trading stocks?", q3_options)
+
+    q4_options = ["1", "2", "3", "4", "5", "More than 5"]
+    q4 = st.radio("4. How many stocks have you invested in before?", q4_options)
+
+    # Question 5: Selectbox
+    q5_options = ["Technology", "Finance", "Healthcare", "Energy"]
+    q5 = st.selectbox("5. Which sector are you most interested in?", q5_options)
+
+    # Button to submit answers
+    if st.button("Submit"):
+
+        # Switch to appropriate section based on answers
+        if q1 == "Yes" and q2 >= 8 and q3 == "Expert":
+            st.success("Switching to Expert section based on your answers...")
+            time.sleep(2)
+            display_expert_section()
+        elif q1 == "Yes" or q2 >= 5:
+            st.success("Switching to Intermediate section based on your answers...")
+            time.sleep(2)
+            display_intermediate_section()
+        else:
+            st.success("Switching to Beginner section based on your answers...")
+            time.sleep(2)
+            display_beginner_section()
+
 
 
 def display_expert_section():
-    get_session_state()
+
     st.markdown("### Welcome to the Expert Section!")
     # Add content specific to the expert section here
     st.write("This section contains resources and tools tailored for experts in trading stocks.")
@@ -160,60 +192,10 @@ def display_beginner_section():
     st.table(df)
 
 
-# Main content area
-st.title("Stocks Web App")
+def stocks():
 
-session_state = get_session_state()
-
-if option == "Home":
-
-    st.write("Please answer the following questions:")
-
-    # Question 1: Radio Buttons
-    q1 = st.radio("1. Do you have prior experience in trading stocks?", ("Yes", "No"))
-
-    # Question 2: Slider
-    q2 = st.slider("2. On a scale of 1 to 10, how confident are you in trading stocks?", 1, 10, 5)
-
-    # Question 3: Radio Buttons
-    q3_options = ["Beginner", "Intermediate", "Expert"]
-    q3 = st.radio("3. What is your level of expertise in trading stocks?", q3_options)
-
-    q4_options = ["1", "2", "3", "4", "5", "More than 5"]
-    q4 = st.radio("4. How many stocks have you invested in before?", q4_options)
-
-    # Question 5: Selectbox
-    q5_options = ["Technology", "Finance", "Healthcare", "Energy"]
-    q5 = st.selectbox("5. Which sector are you most interested in?", q5_options)
-
-    # Button to submit answers
-    if st.button("Submit"):
-
-        # Switch to appropriate section based on answers
-        if q1 == "Yes" and q2 >= 8 and q3 == "Expert":
-            st.success("Based on your answers, you should refer to the expert section.")
-        elif q1 == "Yes" or q2 >= 5:
-            st.success("Based on your answers, you should refer to the intermediate section.")
-        else:
-            st.success("Based on your answers, you should refer to the Beginner section.")
-
-
-elif option == "Beginner":
-    display_beginner_section()
-
-elif option == "Intermediate":
-    display_intermediate_section()
-
-elif option == "Expert":
-    display_expert_section()
-
-
-
-if option2 == "Stocks":
     START = "2015-01-01"
     TODAY = date.today().strftime("%Y-%m-%d")
-
-    st.title('Stock Forecast App')
 
     stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
     selected_stock = st.selectbox('Select dataset for prediction', stocks)
@@ -282,5 +264,144 @@ if option2 == "Stocks":
     fig4.update_scenes(zaxis_autorange="reversed")
     st.plotly_chart(fig4)
 
-if option2 == "Map":
-    st.text_input("Enter your")
+
+def map():
+
+    # Create a sidebar to select map type
+    map_type = "OpenStreetMap"
+
+    # Set default country name
+    default_country = "United States"
+    country_name = st.sidebar.selectbox("Select Country:", ["United States", "Canada", "India", "UK", "China"], index=0)
+
+    # Geocode the country name to get coordinates
+    lat, lon = geocode_country(country_name)
+
+    if lat is not None and lon is not None:
+        # Get zoom level from user
+        zoom_level = st.sidebar.slider("Zoom Level", min_value=1, max_value=20, value=4)
+
+        # Create the map
+        my_map = folium.Map(location=[lat, lon], zoom_start=zoom_level, tiles=map_type)
+
+        # Display the map
+        folium_static(my_map)
+    else:
+        st.error("Invalid country name. Please enter a valid country name.")
+
+    if country_name == "United States":
+        st.write("Top 10 Stocks in the United States:")
+        for stock in get_us_stocks():
+            st.write(f"- {stock}")
+
+    if country_name == "Canada":
+        st.write("Top 10 Stocks in Canada:")
+        for stock in get_canada_stocks():
+            st.write(f"- {stock}")
+
+    if country_name == "India":
+        st.write("Top 10 Stocks in India:")
+        for stock in get_india_stocks():
+            st.write(f"- {stock}")
+
+    if country_name == "UK":
+        st.write("Top 10 Stocks in the UK:")
+        for stock in get_uk_stocks():
+            st.write(f"- {stock}")
+
+    if country_name == "China":
+        st.write("Top 10 Stocks in China:")
+        for stock in get_china_stocks():
+            st.write(f"- {stock}")
+
+
+def get_us_stocks():
+    return [
+        "Apple Inc.", "Microsoft Corporation", "Amazon.com Inc.", "Alphabet Inc. (Class A)",
+        "Facebook Inc.", "JPMorgan Chase & Co.", "Visa Inc.", "Procter & Gamble Company",
+        "The Walt Disney Company", "The Coca-Cola Company"
+    ]
+
+def get_canada_stocks():
+    return [
+        "Toronto-Dominion Bank", "Royal Bank of Canada", "Bank of Nova Scotia", "Bank of Montreal",
+        "Enbridge Inc.", "TC Energy Corporation", "Canadian Imperial Bank of Commerce",
+        "Sun Life Financial Inc.", "Canadian Natural Resources Limited", "Suncor Energy Inc."
+    ]
+
+def get_india_stocks():
+    return [
+        "Reliance Industries Limited", "Tata Consultancy Services Limited", "HDFC Bank Limited",
+        "Infosys Limited", "Hindustan Unilever Limited", "Kotak Mahindra Bank Limited",
+        "ICICI Bank Limited", "State Bank of India", "ITC Limited", "Asian Paints Limited"
+    ]
+
+def get_uk_stocks():
+    return [
+        "HSBC Holdings plc", "Unilever PLC", "BP p.l.c.", "British American Tobacco p.l.c.",
+        "AstraZeneca PLC", "GlaxoSmithKline plc", "Rio Tinto plc", "BHP Group PLC",
+        "Barclays PLC", "Lloyds Banking Group plc"
+    ]
+
+def get_china_stocks():
+    return [
+        "Industrial and Commercial Bank of China Limited", "PetroChina Company Limited",
+        "Agricultural Bank of China Limited", "Bank of China Limited",
+        "China Merchants Bank Co., Ltd.", "China Petroleum & Chemical Corporation",
+        "Kweichow Moutai Co., Ltd.", "China Pacific Insurance (Group) Co., Ltd.",
+        "Jiangsu Hengrui Medicine Co., Ltd.", "China Petroleum & Chemical Corporation"
+    ]
+
+
+def geocode_country(country):
+    url = f"https://nominatim.openstreetmap.org/search?country={country}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        lat = float(data[0]['lat'])
+        lon = float(data[0]['lon'])
+        return lat, lon
+    else:
+        return "25.7617", "80.1918"
+
+
+def menu():
+
+    # Menu
+    with st.sidebar:
+        selected=option_menu(
+            menu_title="Menu",
+            options = ["Home", "Beginner", "Intermediate", "Expert", "Stocks Analyze", "Stock Map"],
+            icons= ["house-heart-fill", "bi bi-currency-exchange", "bi bi-coin", "bi bi-bar-chart-line", "bi bi-clipboard-data", "bi bi-map"],
+            menu_icon= "heart-eyes-fill",
+            default_index= 0,
+        )
+
+    if selected == "Home":
+        st.title(f"Welcome to the {selected} page")
+        home()
+
+    if selected == "Beginner":
+        st.title(f"Welcome to the {selected} page")
+        display_beginner_section()
+
+    if selected == "Intermediate":
+        st.title(f"Welcome to the {selected} page")
+        display_intermediate_section()
+
+    if selected == "Expert":
+        st.title(f"{selected}")
+        display_expert_section()
+
+    if selected == "Stocks Analyze":
+        st.title("Welcome to the stock analyzation page")
+        stocks()
+
+    if selected == "Stock Map":
+        st.title("Welcome to the Stock Map page")
+        map()
+
+
+# Main content area
+st.title("Stocks Web App")
+menu()
